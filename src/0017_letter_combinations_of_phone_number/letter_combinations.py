fd96087fcc431c7d1c55a432eb1723eb7ccfb986
Oleksandr Zinchenko <zinchenko@live.com>
2019-03-18 11:56:10 -0700

    Letter Combination of Phone Number: a minor optimization for backtracking

--- src/0017_letter_combinations_of_phone_number/letter_combinations.py
+++ src/0017_letter_combinations_of_phone_number/letter_combinations.py
@@ -13,29 +13,38 @@ num_map = [
     ['w', 'x', 'y', 'z']
 ]
 
+
 def letterCombinations2(digits: str) -> List[str]:
     result = []
     for digit in reversed(digits):
         if len(result) == 0:
             result = list(num_map[int(digit)])
         else:
-            result = [char + combination for char in num_map[int(digit)] for combination in result]
+            result = [
+                char + combination for char in num_map[int(digit)] for combination in result]
     return result
 
 # backtracking
+
+
 def letterCombinations(digits: str) -> List[str]:
+    if not digits:
+        return []
+
     result = []
-    backtrack([], digits, result)
+
+    def backtrack(combination: str, next_digits: str):
+        if len(next_digits) == 0:
+            result.append(combination)
+            return
+        for char in num_map[int(next_digits[0])]:
+            backtrack(combination + char, next_digits[1:])
+
+    backtrack('', digits)
     return result
 
-def backtrack(combination: List[str], next_digits: str, result: List[str]):
-    if len(next_digits) == 0:
-        if combination != []:
-            result.append(''.join(combination))
-        return
-    for char in num_map[int(next_digits[0])]:
-        backtrack(combination + [char], next_digits[1:], result)
 
 if __name__ == '__main__':
-    assert letterCombinations('23') == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+    assert letterCombinations(
+        '23') == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
     print('passed')
