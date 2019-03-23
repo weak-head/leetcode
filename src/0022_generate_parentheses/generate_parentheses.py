16e637ba4912fffccf2456bdc3322e51fe406912
Oleksandr Zinchenko <zinchenko@live.com>
2019-03-23 09:59:34 -0700

    Generate Parentheses using dynamic programming

--- src/0022_generate_parentheses/generate_parentheses.py
+++ src/0022_generate_parentheses/generate_parentheses.py
@@ -107,17 +107,57 @@ def gen_par_back(s: str, n: int, op: int, cp: int, result: List[str]) -> None:
 # ----------------------------------------------------
 # -- Using Dynamic Programming (easy to follow)
 
+
+def generateParenthesis3(n: int) -> List[str]:
+    pars = {0:set(), 1:{'()'}}
+
+    def gen_par_dyn(n):
+        if n in pars:
+            return pars[n]
+
+        res = set()
+        for k in range(1, n):
+            res.update([lp + rp
+                            for lp in gen_par_dyn(n-k)
+                            for rp in gen_par_dyn(k)])
+
+        for par_per in gen_par_dyn(n-1):
+            res.add('(' + par_per + ')')
+
+        pars[n] = res
+        return res
+
+    return gen_par_dyn(n)
+
 # ---
 # ---
 
 
 if __name__ == '__main__':
     for num in range(9):
+        # a is valid solution
         a = generateParenthesis(num)
         b = generateParenthesis2(num)
+        c = generateParenthesis3(num)
+
         if len(a) != len(b):
-            print(num + ' fail (len): ', len(a), len(b))
-        for ar in a:
-            if ar not in b:
-                print(num + ' fail: ', ar)
-    print('ok')
+            print('[b] failed for num {} len: {} vs {}'.format(num, len(a), len(b)))
+        if len(a) != len(c):
+            print('[c] failed for num {} len: {} vs {}'.format(num, len(a), len(c)))
+
+        sa = set(a)
+        sb = set(b)
+        sc = set(c)
+
+        if sa.difference(sb) != set():
+            print('[a-b] diff: {}'.format(sa - sb))
+            print('[a]: {}'.format(sa))
+            print('[b]: {}'.format(sb))
+            assert False
+        if sa.difference(sc) != set():
+            print('[a-c] diff: {}'.format(sa - sc))
+            print('[a]: {}'.format(sa))
+            print('[c]: {}'.format(sc))
+            assert False
+
+    print('passed')
