30842965fa41f6714d5ee0a80c7e86a4963e002e
Oleksandr Zinchenko <zinchenko@live.com>
2019-03-24 23:28:00 -0700

    Remove Duplicates from Sorted Array

---
+++ src/0023_merge_k_lists/0024_remove_duplicates/remove_duplicates.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def removeDuplicates(nums: List[int]) -> int:
+    if nums == []:
+        return 0
+    u_ix = 0
+    for c_ix in range(len(nums)):
+        if nums[u_ix] != nums[c_ix]:
+            u_ix = u_ix + 1
+            nums[u_ix] = nums[c_ix]
+    return u_ix + 1
