fb8fe4cc29b1258c0dea4ecb8043f3436afd6d0c
Oleksandr Zinchenko <zinchenko@live.com>
2019-04-01 12:40:49 -0700

    Divide Two Integers: couple of test cases

--- src/0029_divide_two_integers/divide_two_integers.py
+++ src/0029_divide_two_integers/divide_two_integers.py
@@ -18,3 +18,22 @@ def divide(dividend: int, divisor: int) -> int:
 
     result = result * sign
     return min((2**31 - 1), max(-2**31, result))
+
+
+if __name__ == '__main__':
+    assert divide(1, 1) == 1
+
+    assert divide(10, 3) == 3
+    assert divide(10, -3) == -3
+    assert divide(-10, 3) == -3
+    assert divide(-10, -3) == 3
+
+    assert divide(1234321, 1) == 1234321
+
+    assert divide(-2**31, -1) == 2**31 - 1
+    assert divide(2**31 - 1, -1) == (-2**31) + 1
+
+    assert divide(2**10, 2) == 2**9
+    assert divide(2**20, 2**10) == 2**10
+
+    print('passed')
