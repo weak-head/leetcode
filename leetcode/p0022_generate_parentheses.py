24ea26941c6d037b57ca25861b637feff3d6fdba
Oleksandr Zinchenko <zinchenko@live.com>
2019-04-15 19:55:48 -0700

    Test cases for p22

--- leetcode/p0022_generate_parentheses.py
+++ leetcode/p0022_generate_parentheses.py
@@ -128,36 +128,3 @@ def generateParenthesis3(n: int) -> List[str]:
         return res
 
     return gen_par_dyn(n)
-
-# ---
-# ---
-
-
-if __name__ == '__main__':
-    for num in range(9):
-        # a is valid solution
-        a = generateParenthesis(num)
-        b = generateParenthesis2(num)
-        c = generateParenthesis3(num)
-
-        if len(a) != len(b):
-            print('[b] failed for num {} len: {} vs {}'.format(num, len(a), len(b)))
-        if len(a) != len(c):
-            print('[c] failed for num {} len: {} vs {}'.format(num, len(a), len(c)))
-
-        sa = set(a)
-        sb = set(b)
-        sc = set(c)
-
-        if sa.difference(sb) != set():
-            print('[a-b] diff: {}'.format(sa - sb))
-            print('[a]: {}'.format(sa))
-            print('[b]: {}'.format(sb))
-            assert False
-        if sa.difference(sc) != set():
-            print('[a-c] diff: {}'.format(sa - sc))
-            print('[a]: {}'.format(sa))
-            print('[c]: {}'.format(sc))
-            assert False
-
-    print('passed')
--- tests/test_p0022_generate_parentheses.py
+++ tests/test_p0022_generate_parentheses.py
@@ -2,36 +2,44 @@ import pytest
 from leetcode.p0022_generate_parentheses import generateParenthesis, generateParenthesis2, generateParenthesis3
 
 
-@pytest.mark.parametrize(('n', 'result'), (
-
+@pytest.mark.parametrize(('num', 'result'), (
+    (0, []),
+    (1, ['()']),
+    (2, ['()()', '(())']),
+    (3, ['((()))', '(()())', '()(())', '(())()', '()()()'])
 ))
-def test_genPar(n, result):
-    pass
+def test_genPar(num, result):
+    a = generateParenthesis(num)
+    b = generateParenthesis2(num)
+    c = generateParenthesis3(num)
 
-    # for num in range(9):
-    #     # a is valid solution
-    #     a = generateParenthesis(num)
-    #     b = generateParenthesis2(num)
-    #     c = generateParenthesis3(num)
+    assert len(result) == len(a)
+    assert len(result) == len(b)
+    assert len(result) == len(c)
 
-    #     if len(a) != len(b):
-    #         print('[b] failed for num {} len: {} vs {}'.format(num, len(a), len(b)))
-    #     if len(a) != len(c):
-    #         print('[c] failed for num {} len: {} vs {}'.format(num, len(a), len(c)))
+    sr = set(result)
+    sa = set(a)
+    sb = set(b)
+    sc = set(c)
 
-    #     sa = set(a)
-    #     sb = set(b)
-    #     sc = set(c)
+    assert sr.difference(sa) == set()
+    assert sr.difference(sb) == set()
+    assert sr.difference(sc) == set()
 
-    #     if sa.difference(sb) != set():
-    #         print('[a-b] diff: {}'.format(sa - sb))
-    #         print('[a]: {}'.format(sa))
-    #         print('[b]: {}'.format(sb))
-    #         assert False
-    #     if sa.difference(sc) != set():
-    #         print('[a-c] diff: {}'.format(sa - sc))
-    #         print('[a]: {}'.format(sa))
-    #         print('[c]: {}'.format(sc))
-    #         assert False
 
-    # print('passed')
+def test_genPar_range():
+    for num in range(9):
+        # a is valid solution
+        a = generateParenthesis(num)
+        b = generateParenthesis2(num)
+        c = generateParenthesis3(num)
+
+        assert len(a) == len(b)
+        assert len(a) == len(c)
+
+        sa = set(a)
+        sb = set(b)
+        sc = set(c)
+
+        assert sa.difference(sb) == set()
+        assert sa.difference(sc) == set()
