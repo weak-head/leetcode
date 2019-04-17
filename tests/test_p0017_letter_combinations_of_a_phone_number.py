f49bff2b13d009a50a4df9ff133cd08e8aff01de
Oleksandr Zinchenko <zinchenko@live.com>
2019-04-17 08:09:23 -0700

    Additional test cases for p17

--- tests/test_p0017_letter_combinations_of_a_phone_number.py
+++ tests/test_p0017_letter_combinations_of_a_phone_number.py
@@ -4,6 +4,9 @@ from leetcode.p0017_letter_combinations_of_a_phone_number import letterCombinati
 
 @pytest.mark.parametrize(('number', 'combinations'), (
     ('23', ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]),
+    ('45', ["gj", "gk", "gl", "hj", "hk", "hl", "ij", "ik", "il"]),
+    ('', [])
 ))
 def test_letterCombinations(number, combinations):
     assert letterCombinations(number) == combinations
+    assert letterCombinations2(number) == combinations
