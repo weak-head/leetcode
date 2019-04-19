0623cbf58fd2a1c3ff0d628f1d1c0809927d03e4
Oleksandr Zinchenko <zinchenko@live.com>
2019-04-19 08:39:49 -0700

    Test cases for p7, p13

--- tests/test_p0007_reverse_integer.py
+++ tests/test_p0007_reverse_integer.py
@@ -6,7 +6,9 @@ from leetcode.p0007_reverse_integer import reverse
     (123, 321),
     (0, 0),
     (9, 9),
-    (-123, -321)
+    (-123, -321),
+    (-2**31 - 1, 0),
+    (2**31, 0)
 ))
 def test_reverse(x, exp):
     assert reverse(x) == exp
--- tests/test_p0013_roman_to_integer.py
+++ tests/test_p0013_roman_to_integer.py
@@ -7,7 +7,22 @@ from leetcode.p0013_roman_to_integer import romanToInt
     ('VII', 7),
     ('IIX', 8),
     ('XVII', 17),
-    ('IVIV', 8)
+    ('IVIV', 8),
+    ('XC', 90),
+    ('MD', 1500),
+    ('MC', 1100),
+    ('CM', 900),
+    ('MDCLXVI', 1666)
 ))
 def test_romanToInt(roman, integer):
     assert romanToInt(roman) == integer
+
+
+@pytest.mark.parametrize('roman', (
+    'ABC',
+    '!@#',
+    'QWE'
+))
+def test_romanToInt_invalid(roman):
+    with pytest.raises(Exception) as ex:
+        romanToInt(roman)
