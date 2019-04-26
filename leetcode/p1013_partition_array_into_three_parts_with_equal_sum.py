04677abe60f36e2bc1360e0957d7382e61c9e460
Oleksandr Zinchenko <zinchenko@live.com>
2019-04-26 08:52:33 -0700

    Partition Array Into Three Parts With Equal Sum

--- leetcode/p1013_partition_array_into_three_parts_with_equal_sum.py
+++ leetcode/p1013_partition_array_into_three_parts_with_equal_sum.py
@@ -1,6 +1,20 @@
 from typing import List
 
 def canThreePartsEqualSum(A: List[int]) -> bool:
-    total_sum = sum(A)
-    lix, rix = 0, len(A) - 1
-    pass
+    if len(A) < 3:
+        return False
+
+    (q, r) = divmod(sum(A), 3)
+    if r != 0:
+        return False
+
+    n, part_sum = 0, 0
+    for x in A:
+        # print(x)
+        part_sum = part_sum + x
+        if part_sum == q:
+            # print(part_sum, '==', q)
+            n = n + 1
+            part_sum = 0
+
+    return (n == 3) or (n > 3 and q == 0)
--- tests/test_p1013_partition_array_into_three_parts_with_equal_sum.py
+++ tests/test_p1013_partition_array_into_three_parts_with_equal_sum.py
@@ -3,6 +3,16 @@ from leetcode.p1013_partition_array_into_three_parts_with_equal_sum import canTh
 
 
 @pytest.mark.parametrize(('a', 'expectation'), (
+    ([0,2,1,-6,6,-7,9,1,2,0,1], True),
+    ([0,2,1,-6,6,7,9,-1,2,0,1], False),
+    ([3,3,6,5,-2,2,5,1,-9,4], True),
+    ([0,0,0,0,0,0], True),
+    ([0,0,0], True),
+    ([0,0], False),
+    ([-1,1,-1,1,-1,1,-1,1,-1,1,-1,1], True),
+    ([-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,1,1,1], True),
+    ([-1,1,1,1,-1,1,-1,1,-1,1,-1,1,1,-1,1], True),
+    ([1,1,1,1,-1,1,-1,1,-1,1,-1,1,-1,-1,1], False),
 ))
 def test_(a, expectation):
     assert canThreePartsEqualSum(a) == expectation
