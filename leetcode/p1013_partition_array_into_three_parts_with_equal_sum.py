18c5071c170e5cb1936c81b10aaf4df11e60efa6
Oleksandr Zinchenko <zinchenko@live.com>
2019-04-25 23:54:38 -0700

    Skeleton for 1013

---
+++ leetcode/p1013_partition_array_into_three_parts_with_equal_sum.py
@@ -0,0 +1,6 @@
+from typing import List
+
+def canThreePartsEqualSum(A: List[int]) -> bool:
+    total_sum = sum(A)
+    lix, rix = 0, len(A) - 1
+    pass
---
+++ tests/test_p1013_partition_array_into_three_parts_with_equal_sum.py
@@ -0,0 +1,8 @@
+import pytest
+from leetcode.p1013_partition_array_into_three_parts_with_equal_sum import canThreePartsEqualSum
+
+
+@pytest.mark.parametrize(('a', 'expectation'), (
+))
+def test_(a, expectation):
+    assert canThreePartsEqualSum(a) == expectation
