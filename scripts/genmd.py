47c3648a868021d4c79c40848a4f5e2889aea2a1
Oleksandr Zinchenko <zinchenko@live.com>
2019-05-06 23:37:00 -0700

    Re-format using flake8 and black

--- scripts/genmd.py
+++ scripts/genmd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Generates markdown table with list of problems'''
+"""Generates markdown table with list of problems"""
 
 import six
 import os
@@ -9,13 +9,13 @@ from pytablewriter import MarkdownTableWriter
 from termcolor import colored
 
 
-TABLE_TITLE = 'List of Problems'
+TABLE_TITLE = "List of Problems"
 
-LEETCODE_URL = 'https://leetcode.com/problems/{}/'
-GITHUB_URL = 'https://github.com/weak-head/leetcode/blob/master/{}'
-GITHUB_SRC = 'leetcode/{}'
-GITHUB_TST = 'tests/test_{}'
-MD_LINK = '[{title}]({link})'
+LEETCODE_URL = "https://leetcode.com/problems/{}/"
+GITHUB_URL = "https://github.com/weak-head/leetcode/blob/master/{}"
+GITHUB_SRC = "leetcode/{}"
+GITHUB_TST = "tests/test_{}"
+MD_LINK = "[{title}]({link})"
 
 
 def verify_url(url, verify=True):
@@ -23,12 +23,16 @@ def verify_url(url, verify=True):
         try:
             r = requests.get(url, timeout=3)
             if r.status_code != 200:
-                raise Exception('broken link')
-        except:
-            print(colored('  Broken link [{status}]: {link}'.format(
-                status=r.status_code,
-                link=url
-            ), 'red'))
+                raise Exception("broken link")
+        except Exception:
+            print(
+                colored(
+                    "  Broken link [{status}]: {link}".format(
+                        status=r.status_code, link=url
+                    ),
+                    "red",
+                )
+            )
             return False
     return True
 
@@ -36,67 +40,53 @@ def verify_url(url, verify=True):
 def verify_file(file_path, verify=True):
     if verify:
         if not os.path.isfile(file_path):
-            print(colored('  Missing file: {path}'.format(
-                path=file_path
-            ), 'yellow'))
+            print(colored("  Missing file: {path}".format(path=file_path), "yellow"))
             return False
     return True
 
 
 def next_problem(id, title, silent=False):
     if not silent:
-        print('\n{id} - {title}:'.format(
-            id=id,
-            title=title
-        ))
+        print("\n{id} - {title}:".format(id=id, title=title))
 
 
 def verification_status(id, title, success, silent=False):
     if success and not silent:
-        print(colored('  OK', 'green'))
+        print(colored("  OK", "green"))
 
 
 def parse_filename(file_name):
-    ''' Filename pattern: pXXXX_<url_part>.py '''
-    id = file_name[1:5]    # XXXX
-    name = file_name[6:-3] # <url_part>
+    """ Filename pattern: pXXXX_<url_part>.py """
+    id = file_name[1:5]  # XXXX
+    name = file_name[6:-3]  # <url_part>
     return id, name
 
 
 def gen_table(verify=False, silent=False):
-    header = ['#', 'Title', 'Solution', 'Test cases']
+    header = ["#", "Title", "Solution", "Test cases"]
     table = []
 
-    for file in os.listdir('./leetcode'):
+    for file in os.listdir("./leetcode"):
         # if problem
-        if file[0] == 'p':
+        if file[0] == "p":
             id, name = parse_filename(file)
-            problem_title = name.replace('_', ' ').title()
+            problem_title = name.replace("_", " ").title()
             next_problem(id, problem_title, silent)
 
             # compose leetcode link
-            leetcode_url = LEETCODE_URL.format(name.replace('_', '-'))
-            leetcode_link = MD_LINK.format(
-                title=problem_title,
-                link=leetcode_url
-            )
+            leetcode_url = LEETCODE_URL.format(name.replace("_", "-"))
+            leetcode_link = MD_LINK.format(title=problem_title, link=leetcode_url)
             success = verify_url(leetcode_url, verify)
 
             # compose github src link
             src_url = GITHUB_SRC.format(file)
-            src_link = MD_LINK.format(
-                title='src',
-                link=src_url
-            )
+            src_link = MD_LINK.format(title="src", link=src_url)
             success = verify_url(GITHUB_URL.format(src_url), verify) and success
             success = verify_file(src_url, verify) and success
 
             # compose github tst link
             tst_url = GITHUB_TST.format(file)
-            tst_link = MD_LINK.format(
-                title='tst',
-                link=tst_url
-            )
+            tst_link = MD_LINK.format(title="tst", link=tst_url)
             success = verify_url(GITHUB_URL.format(tst_url), verify) and success
             success = verify_file(tst_url, verify) and success
 
@@ -121,30 +111,30 @@ def to_markdown(title, header, table):
 
 
 def refresh_markdown(file_name, verify=False, silent=False):
-    content = ''
-    with open(file_name, 'r') as readme:
+    content = ""
+    with open(file_name, "r") as readme:
         content = readme.read()
 
     # Drop the old table
     title_loc_ix = content.find(TABLE_TITLE)
-    content = content[:title_loc_ix-2] # drop '# '
+    content = content[: title_loc_ix - 2]  # drop '# '
 
     # Generate a new one
     header, table = gen_table(verify, silent)
     markdown = to_markdown(TABLE_TITLE, header, table)
 
     content = content + markdown
-    with open(file_name, 'w') as readme:
+    with open(file_name, "w") as readme:
         readme.write(content)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('--verify', action='store_true', help='verify links')
+    parser.add_argument("--verify", action="store_true", help="verify links")
     args = parser.parse_args()
 
     try:
-        refresh_markdown('README.md', args.verify, False)
+        refresh_markdown("README.md", args.verify, False)
         print()
-    except:
-        print('\nterminated\n')
+    except Exception:
+        print("\nterminated\n")
--- scripts/touch.py
+++ scripts/touch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Creates the template for a new problem'''
+"""Creates the template for a new problem"""
 
 import os
 import requests
@@ -16,20 +16,17 @@ def extract_title(url):
     path = urlparse(url).path
 
     # drop trailing slash
-    path = path[:-1] if path[-1] == '/' else path
+    path = path[:-1] if path[-1] == "/" else path
     return os.path.split(path)[-1]
 
 
-def file_name(id, title, kind='src'):
-    ''' Generates file name from problem id and title '''
-    fn_template = 'p{id:0>4d}_{title}.py'
-    if kind == 'tst':
-        fn_template = 'test_' + fn_template
+def file_name(id, title, kind="src"):
+    """ Generates file name from problem id and title """
+    fn_template = "p{id:0>4d}_{title}.py"
+    if kind == "tst":
+        fn_template = "test_" + fn_template
 
-    return fn_template.format(
-        id=id,
-        title=title.replace('-', '_')
-    )
+    return fn_template.format(id=id, title=title.replace("-", "_"))
 
 
 def verify_url(url):
@@ -38,11 +35,9 @@ def verify_url(url):
             # TODO: verify this url belongs to leetcode domain
             r = requests.get(url, timeout=3)
             if r.status_code != 200:
-                raise Exception('bad url')
-        except:
-            print(colored('Broken link: {link}'.format(
-                link=url
-            ), 'red'))
+                raise Exception("bad url")
+        except Exception:
+            print(colored("Broken link: {link}".format(link=url), "red"))
             return False
     return True
 
@@ -50,20 +45,18 @@ def verify_url(url):
 def verify_path(file_path):
     if VERIFY:
         if os.path.isfile(file_path):
-            print(colored('Already exists: {file}'.format(
-                file=file_path
-            ), 'red'))
+            print(colored("Already exists: {file}".format(file=file_path), "red"))
             return False
     return True
 
 
 def src_template(id, title):
-    return ''
+    return ""
 
 
 def tst_template(id, title):
     return (
-'''import pytest
+        """import pytest
 from leetcode.{src} import {method}
 
 
@@ -72,17 +65,16 @@ from leetcode.{src} import {method}
 ))
 def test_(a, expectation):
     assert {method}(a) == expectation
-'''
+"""
     ).format(
-        src=file_name(id, title, 'src')[:-3], # without .py extension
-        method='solve'
+        src=file_name(id, title, "src")[:-3], method="solve"  # without .py extension
     )
 
 
 def create_files(id, url):
     title = extract_title(url)
-    src = os.path.join('leetcode', file_name(id, title, 'src'))
-    tst = os.path.join('tests', file_name(id, title, 'tst'))
+    src = os.path.join("leetcode", file_name(id, title, "src"))
+    tst = os.path.join("tests", file_name(id, title, "tst"))
 
     valid = verify_url(url)
     valid = verify_path(src) and valid
@@ -91,22 +83,26 @@ def create_files(id, url):
     if not valid:
         return False
 
-    with open(src, 'w') as f:
+    with open(src, "w") as f:
         f.write(src_template(id, title))
-        print(colored('Created {}'.format(src), 'green'))
+        print(colored("Created {}".format(src), "green"))
 
-    with open(tst, 'w') as f:
+    with open(tst, "w") as f:
         f.write(tst_template(id, title))
-        print(colored('Created {}'.format(tst), 'green'))
+        print(colored("Created {}".format(tst), "green"))
 
     return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('--skip-verification', action='store_true', help='skip file and url verification')
-    parser.add_argument('id')
-    parser.add_argument('url')
+    parser.add_argument(
+        "--skip-verification",
+        action="store_true",
+        help="skip file and url verification",
+    )
+    parser.add_argument("id")
+    parser.add_argument("url")
     args = parser.parse_args()
 
     VERIFY = not args.skip_verification
@@ -119,7 +115,7 @@ if __name__ == '__main__':
         url = args.url
 
         if create_files(id, url):
-            refresh_markdown('README.md', False, True)
-            print(colored('Updated README.md\n', 'green'))
+            refresh_markdown("README.md", False, True)
+            print(colored("Updated README.md\n", "green"))
     except Exception as e:
-        print(colored('\nfailed: {}\n'.format(e), 'red'))
+        print(colored("\nfailed: {}\n".format(e), "red"))
