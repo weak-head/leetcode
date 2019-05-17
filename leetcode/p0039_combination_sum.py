90494a3f3b90dc043c7f53d233f28b46fa9f3f19
Oleksandr Zinchenko <zinchenko@live.com>
2019-05-16 20:34:04 -0700

    Combination sum, optimized backtracking

--- leetcode/p0039_combination_sum.py
+++ leetcode/p0039_combination_sum.py
@@ -36,7 +36,8 @@ def add_new(n, target, combination, combs):
 
 
 def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
-    """Backtracking but still very slow"""
+    """Backtracking that uses set to store
+    unique combinations. Pretty slow."""
     result = set()
     backtrack(target, (), candidates, result)
     return list(result)
@@ -51,3 +52,30 @@ def backtrack(target, comb, candidates, result):
 
     for c in candidates:
         backtrack(target - c, tuple(sorted((comb + (c,)))), candidates, result)
+
+
+# -------------------------
+
+
+def combinationSum3(candidates: List[int], target: int) -> List[List[int]]:
+    """Optimized backtracking that excludes each
+    processed item and doesn't require to maintain
+    set of unique combinations
+    """
+    result = []
+    backtrack3(target, [], candidates, result)
+    return result
+
+
+def backtrack3(target, comb, candidates, result):
+    if target == 0:
+        result.append(comb)
+
+    if target < 0:
+        return
+
+    cnd = list(candidates)
+    while len(cnd) > 0:
+        c = cnd[0]
+        backtrack3(target - c, comb + [c], cnd, result)
+        cnd.pop(0)
--- tests/test_p0039_combination_sum.py
+++ tests/test_p0039_combination_sum.py
@@ -1,19 +1,34 @@
 import pytest
-from leetcode.p0039_combination_sum import combinationSum, combinationSum2
-
-
-@pytest.mark.parametrize(
-    ("arr", "target", "expectation"),
-    (([2, 3, 5], 8, [(2, 2, 2, 2), (2, 3, 3), (3, 5)]),),
+from leetcode.p0039_combination_sum import (
+    combinationSum,
+    combinationSum2,
+    combinationSum3,
 )
+
+test_cases = (
+    ([2, 3, 5], 8, {(2, 2, 2, 2), (2, 3, 3), (3, 5)}),
+    ([2, 3, 6, 7], 7, {(7,), (2, 2, 3)}),
+)
+
+
+@pytest.mark.parametrize(("arr", "target", "expectation"), test_cases)
 def test_combination(arr, target, expectation):
     verify(arr, target, expectation, combinationSum)
+
+
+@pytest.mark.parametrize(("arr", "target", "expectation"), test_cases)
+def test_combination2(arr, target, expectation):
     verify(arr, target, expectation, combinationSum2)
 
 
+@pytest.mark.parametrize(("arr", "target", "expectation"), test_cases)
+def test_combination3(arr, target, expectation):
+    verify(arr, target, expectation, combinationSum3)
+
+
 def verify(arr, target, expectation, func):
-    result = set(func(arr, target))
+    result = func(arr, target)
     assert len(result) == len(expectation)
 
-    for e in expectation:
-        assert e in result
+    for r in result:
+        assert tuple(sorted(r)) in expectation
