79601b755f88adb24cad3fa97b23689a86b48939
Oleksandr Zinchenko <zinchenko@live.com>
2019-05-16 23:20:03 -0700

    Combination Sum with optimization

--- leetcode/p0040_combination_sum_ii.py
+++ leetcode/p0040_combination_sum_ii.py
@@ -3,15 +3,11 @@ from typing import List
 
 def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
     result = []
-    backtrack([], target, candidates, result)
-
-    ur = set()
-    for r in result:
-        ur.add(tuple(sorted(r)))
-    return list(ur)
+    backtrack([], target, 0, sorted(candidates), result)
+    return result
 
 
-def backtrack(seq, target, candidates, result):
+def backtrack(seq, target, index, candidates, result):
     if target < 0:
         return
 
@@ -19,9 +15,13 @@ def backtrack(seq, target, candidates, result):
         result.append(seq)
         return
 
-    cnd = list(candidates)
-    while cnd:
-        c = cnd[0]
-        cnd = cnd[1:]
-        if c <= target:
-            backtrack(seq + [c], target - c, cnd, result)
+    for i in range(index, len(candidates)):
+        num = candidates[i]
+
+        if i > index and candidates[i - 1] == candidates[i]:
+            continue
+
+        if num > target:
+            break
+
+        backtrack(seq + [num], target - num, i + 1, candidates, result)
