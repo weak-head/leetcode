3b8155f750357a798c9af345ac32553b6d72b6c6
Oleksandr Zinchenko <zinchenko@live.com>
2019-05-21 22:42:15 -0700

    Additional test cases for First Missing Number

--- tests/test_p0041_first_missing_positive.py
+++ tests/test_p0041_first_missing_positive.py
@@ -9,6 +9,8 @@ from leetcode.p0041_first_missing_positive import firstMissingPositive
         ([-1, 2, 3, 5, 1, 2, 0, 9, 6, 7], 4),
         ([3, 4, -1, 1], 2),
         ([1, 2, 0], 3),
+        ([], 1),
+        ([3, 5, 4, 2, 1, 6, 9, 7, 8], 10),
     ),
 )
 def test_firstMissing(a, expectation):
