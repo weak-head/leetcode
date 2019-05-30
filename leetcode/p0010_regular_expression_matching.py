5ea5190a74a89cf718ef4bbee45dfa7f9cd1f614
Oleksandr Zinchenko <zinchenko@live.com>
2019-05-30 15:06:48 -0700

    Regular expression matching

--- leetcode/p0010_regular_expression_matching.py
+++ leetcode/p0010_regular_expression_matching.py
@@ -1,12 +1,15 @@
-
 def isMatch(s: str, p: str) -> bool:
-    if s == '' and p == '':
+    """
+    Backtracking without memoization,
+    with string copy. Very slow.
+    """
+    if s == "" and p == "":
         return True
-    if p == '':
+    if p == "":
         return False
 
-    if s != '' and (s[0] == p[0] or p[0] == '.'):
-        if len(p) > 1 and p[1] == '*':
+    if s != "" and (s[0] == p[0] or p[0] == "."):
+        if len(p) > 1 and p[1] == "*":
             # ignore glob
             match = isMatch(s, p[2:])
 
@@ -19,7 +22,46 @@ def isMatch(s: str, p: str) -> bool:
             return isMatch(s[1:], p[1:])
     # s == '' | s[0] != p[0] | p[0] != '.'
     else:
-        if len(p) > 1 and p[1] == '*':
+        if len(p) > 1 and p[1] == "*":
             return isMatch(s, p[2:])
 
     return False
+
+
+def isMatch2(s, p):
+    """Backtracking with memoization"""
+    sn, pn = len(s), len(p)
+    si = pi = 0
+    stack, tried = list(), set()
+
+    # try to reach the end of the string
+    while si < sn:
+        if pi + 1 < pn and p[pi + 1] == "*":
+            # here we have two cases:
+            #   - consume string character
+            #   - ignore pattern
+            nexttry = (si + 1, pi)
+            if p[pi] in {s[si], "."} and nexttry not in tried:
+                # consume the character from 's'
+                # and memorize that we need to try this later
+                stack.append(nexttry)
+                tried.add(nexttry)
+
+            # ignore pattern
+            pi += 2
+        elif pi < pn and p[pi] in {s[si], "."}:
+            # exact match
+            pi += 1
+            si += 1
+        elif stack:
+            # backtrack
+            si, pi = stack.pop()
+        else:
+            return False
+
+    # after we reached the end of the string
+    # we can ignore all kleene stars
+    while pi + 1 < pn and p[pi + 1] == "*":
+        pi += 2
+
+    return pi >= pn
--- tests/test_p0010_regular_expression_matching.py
+++ tests/test_p0010_regular_expression_matching.py
@@ -1,35 +1,34 @@
 import pytest
-from leetcode.p0010_regular_expression_matching import isMatch
+from leetcode.p0010_regular_expression_matching import isMatch, isMatch2
+
 
 @pytest.mark.timeout(10)
-@pytest.mark.parametrize(('string', 'pattern', 'match'), (
-    ('aaa', 'aaa', True),
-    ('aaa', 'a*', True),
-
-    ('aaa', 'aaaa', False),
-    ('aaaa', 'aaa', False),
-
-    ('', '.*', True),
-    ('', 'a*', True),
-
-    ('abc', 'c*d*abb*cc*', True),
-    ('abc', 'c*d*a.b*cc*', True),
-    ('abcde', 'c*d*a.c.*', True),
-    ('abcde', 'c*d*a.b.*', False),
-    ('abc', '.*', True),
-
-    ('', '', True),
-    ('abc', '', False),
-    ('', 'abc', False),
-    ('', 'a*b*c*', True),
-
-    ('abcde', '.....', True),
-    ('abcdef', '.....', False),
-    ('abcd', '.....', False),
-
-    ('bbbbba', '.*a*a', True),
-
-    #("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c", False)
-))
-def test_isMatch(string, pattern, match):
-    assert isMatch(string, pattern) == match
+@pytest.mark.parametrize(
+    ("string", "pattern", "match", "both"),
+    (
+        ("aaa", "aaa", True, True),
+        ("aaa", "a*", True, True),
+        ("aaa", "aaaa", False, True),
+        ("aaaa", "aaa", False, True),
+        ("", ".*", True, True),
+        ("", "a*", True, True),
+        ("abc", "c*d*abb*cc*", True, True),
+        ("abc", "c*d*a.b*cc*", True, True),
+        ("abcde", "c*d*a.c.*", True, True),
+        ("abcde", "c*d*a.b.*", False, True),
+        ("abc", ".*", True, True),
+        ("", "", True, True),
+        ("abc", "", False, True),
+        ("", "abc", False, True),
+        ("", "a*b*c*", True, True),
+        ("abcde", ".....", True, True),
+        ("abcdef", ".....", False, True),
+        ("abcd", ".....", False, True),
+        ("bbbbba", ".*a*a", True, True),
+        ("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c", False, False),
+    ),
+)
+def test_isMatch(string, pattern, match, both):
+    if both:
+        assert isMatch(string, pattern) == match
+    assert isMatch2(string, pattern) == match
