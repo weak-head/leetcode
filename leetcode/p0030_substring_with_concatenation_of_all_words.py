ef5c03a68db55b1e94ca766dec413099b1808043
Oleksandr Zinchenko <zinchenko@live.com>
2019-05-31 08:34:48 -0700

    Substring with concatenation using sliding window

--- leetcode/p0030_substring_with_concatenation_of_all_words.py
+++ leetcode/p0030_substring_with_concatenation_of_all_words.py
@@ -38,12 +38,63 @@ def findSubstring(s: str, words: List[str]) -> List[int]:
     return matches
 
 
+def findSubstring2(s, words):
+    """
+    Using sliding window.
+    """
+    if not s or not words:
+        return []
+
+    sn = len(s)
+    wn, wl = len(words[0]), len(words)
+    wset = Counter(words)
+    res = []
+
+    offsets = {}
+    for ix in range(0, wn):
+        offsets[ix] = wset.copy()
+        for wi in range(len(words)):
+            word = s[ix + (wi * wn) : ix + (wi * wn) + wn]
+            if word in wset:
+                offsets[ix][word] -= 1
+
+        if isMatch(ix, offsets):
+            res.append(ix)
+
+    si = wn
+    while si < sn:
+        offset = si % wn
+        prev_word = s[si - wn : si]
+        next_word = s[si + (wn * wl) - wn : si + (wn * wl)]
+
+        if prev_word in wset:
+            offsets[offset][prev_word] += 1
+
+        if next_word in wset:
+            offsets[offset][next_word] -= 1
+
+        if isMatch(offset, offsets):
+            res.append(si)
+
+        si += 1
+
+    return res
+
+
+def isMatch(ix, d):
+    for val in d[ix].values():
+        if val != 0:
+            return False
+    return True
+
+
 if __name__ == "__main__":
-    print(findSubstring("barfoothefoobarman", ["foo", "bar"]))
-    print(findSubstring("wordgoodgoodgoodbestword", ["word", "good", "best", "word"]))
-    print(findSubstring("barfoofoobarthefoobarman", ["bar", "foo", "the"]))
+    # print(findSubstring2("barfoothefoobarman", ["foo", "bar"]))
+    # print(findSubstring2("wordgoodgoodgoodbestword", ["word", "good", "best", "word"]))
+    # print(findSubstring("barfoofoobarthefoobarman", ["bar", "foo", "the"]))
+    # print(findSubstring2("barfoofoobarthefoobarman", ["bar", "foo", "the"]))
     print(
-        findSubstring(
+        findSubstring2(
             "lingmindraboofooowingdingbarrwingmonkeypoundcake",
             ["fooo", "barr", "wing", "ding", "wing"],
         )
