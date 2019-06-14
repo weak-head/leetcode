44d309a0f56dfe659e968fcdf5346569d56a9020
Oleksandr Zinchenko <zinchenko@live.com>
2019-06-13 22:40:31 -0700

    Jump Game II linear time solution

--- tests/test_p0045_jump_game_ii.py
+++ tests/test_p0045_jump_game_ii.py
@@ -1,10 +1,21 @@
 import pytest
-from leetcode.p0045_submissions import jump
+from leetcode.p0045_jump_game_ii import jump, jump2
 
 
 @pytest.mark.parametrize(
     ("input", "res"),
-    (([2, 3, 1, 1, 4], 2), ([], 0), ([1, 2, 1, 3, 1, 1, 2, 1, 1, 7], 5)),
+    (
+        ([2, 3, 1, 1, 4], 2),
+        ([], 0),
+        ([1, 2, 1, 3, 1, 1, 2, 1, 1, 7], 5),
+        ([3, 2, 1], 1),
+        ([1, 2], 1),
+        ([2, 3, 1], 1),
+        ([5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0], 3),
+        ([1], 0),
+        ([1, 2, 3], 2),
+    ),
 )
 def test_jump(input, res):
     assert jump(input) == res
+    assert jump2(input) == res
