86b621c64ddda35832e6aef27fdc953a3cd88007
Oleksandr Zinchenko <zinchenko@live.com>
2019-08-19 14:00:32 -0700

    Length of longest substring, implementation that is easier to follow

--- leetcode/p0003_longest_substring_without_repeating_characters.py
+++ leetcode/p0003_longest_substring_without_repeating_characters.py
@@ -1,4 +1,3 @@
-
 def lengthOfLongestSubstring(s: str) -> int:
     visited_chars, max_length, start_ix = {}, 0, 0
     for end_ix, char in enumerate(s):
@@ -11,3 +10,39 @@ def lengthOfLongestSubstring(s: str) -> int:
             max_length = max(max_length, end_ix - start_ix + 1)
         visited_chars[char] = end_ix
     return max(max_length, len(s) - start_ix)
+
+
+def lengthOfLongestSubstring2(s: str) -> int:
+    subs = {}
+    max_len, l_ix = 0, 0
+
+    for ix, char in enumerate(s):
+
+        # the character is not in the substring
+        # we can safely add it and increase the
+        # length of current substring
+        if char not in subs:
+            max_len = max(max_len, ix - l_ix + 1)
+
+        # the character is in substring,
+        # we have to adjust the substring,
+        # removing all left items.
+        # but there is an optimization we can make.
+        # instead of touching the dictionary we can work
+        # with indexes and check if the index of the character
+        # is within the current substring range
+        else:
+            last_char_ix = subs[char]
+
+            # the char is outside the substring
+            # borders and we can simply ignore it
+            if last_char_ix < l_ix:
+                max_len = max(max_len, ix - l_ix + 1)
+
+            # the previous char position is our current left limit
+            else:
+                l_ix = last_char_ix + 1
+
+        subs[char] = ix
+
+    return max_len
--- tests/test_p0003_longest_substring_without_repeating_characters.py
+++ tests/test_p0003_longest_substring_without_repeating_characters.py
@@ -1,16 +1,23 @@
 import pytest
-from leetcode.p0003_longest_substring_without_repeating_characters import lengthOfLongestSubstring
+from leetcode.p0003_longest_substring_without_repeating_characters import (
+    lengthOfLongestSubstring,
+    lengthOfLongestSubstring2,
+)
 
 
-@pytest.mark.parametrize(('s', 'expectation'), (
-    ('abcd', 4),
-    ('aaaaa', 1),
-    ('a', 1),
-    ('', 0),
-    ('abcabcdabcdeabc', 5),
-    ('ababbbbaaabba', 2),
-    ('ababbbbaaacbbbaabaaac', 3),
-    ('tmmzuxt', 5)
-))
+@pytest.mark.parametrize(
+    ("s", "expectation"),
+    (
+        ("abcd", 4),
+        ("aaaaa", 1),
+        ("a", 1),
+        ("", 0),
+        ("abcabcdabcdeabc", 5),
+        ("ababbbbaaabba", 2),
+        ("ababbbbaaacbbbaabaaac", 3),
+        ("tmmzuxt", 5),
+    ),
+)
 def test_lengthOfSubstring(s, expectation):
     assert lengthOfLongestSubstring(s) == expectation
+    assert lengthOfLongestSubstring2(s) == expectation
