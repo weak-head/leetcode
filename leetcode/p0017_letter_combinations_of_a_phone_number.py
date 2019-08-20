32a6432b563d09b88aff0b78fcc2c2d4f5c12f0d
Oleksandr Zinchenko <zinchenko@live.com>
2019-08-20 15:14:16 -0700

    Simplified backtracking for letter combination

--- leetcode/p0017_letter_combinations_of_a_phone_number.py
+++ leetcode/p0017_letter_combinations_of_a_phone_number.py
@@ -3,14 +3,14 @@ from typing import List
 num_map = [
     [],
     [],
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f'],
-    ['g', 'h', 'i'],
-    ['j', 'k', 'l'],
-    ['m', 'n', 'o'],
-    ['p', 'q', 'r', 's'],
-    ['t', 'u', 'v'],
-    ['w', 'x', 'y', 'z']
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"],
+    ["j", "k", "l"],
+    ["m", "n", "o"],
+    ["p", "q", "r", "s"],
+    ["t", "u", "v"],
+    ["w", "x", "y", "z"],
 ]
 
 
@@ -21,8 +21,12 @@ def letterCombinations2(digits: str) -> List[str]:
             result = list(num_map[int(digit)])
         else:
             result = [
-                char + combination for char in num_map[int(digit)] for combination in result]
+                char + combination
+                for char in num_map[int(digit)]
+                for combination in result
+            ]
     return result
+
 
 # backtracking
 
@@ -40,5 +44,27 @@ def letterCombinations(digits: str) -> List[str]:
         for char in num_map[int(next_digits[0])]:
             backtrack(combination + char, next_digits[1:])
 
-    backtrack('', digits)
+    backtrack("", digits)
     return result
+
+
+def letterCombinations3(digits: str) -> List[str]:
+    if not digits:
+        return []
+
+    num_map = [None, None, "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+
+    def backtrack(nums):
+        if len(nums) == 1:
+            return num_map[int(nums[0])]
+
+        tails = backtrack(nums[1:])
+
+        result = []
+        for char in num_map[int(nums[0])]:
+            for tail in tails:
+                result.append(char + tail)
+
+        return result
+
+    return backtrack(digits)
--- tests/test_p0017_letter_combinations_of_a_phone_number.py
+++ tests/test_p0017_letter_combinations_of_a_phone_number.py
@@ -1,12 +1,20 @@
 import pytest
-from leetcode.p0017_letter_combinations_of_a_phone_number import letterCombinations, letterCombinations2
+from leetcode.p0017_letter_combinations_of_a_phone_number import (
+    letterCombinations,
+    letterCombinations2,
+    letterCombinations3,
+)
 
 
-@pytest.mark.parametrize(('number', 'combinations'), (
-    ('23', ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]),
-    ('45', ["gj", "gk", "gl", "hj", "hk", "hl", "ij", "ik", "il"]),
-    ('', [])
-))
+@pytest.mark.parametrize(
+    ("number", "combinations"),
+    (
+        ("23", ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]),
+        ("45", ["gj", "gk", "gl", "hj", "hk", "hl", "ij", "ik", "il"]),
+        ("", []),
+    ),
+)
 def test_letterCombinations(number, combinations):
     assert letterCombinations(number) == combinations
     assert letterCombinations2(number) == combinations
+    assert letterCombinations3(number) == combinations
