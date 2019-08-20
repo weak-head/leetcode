afbcff3655771c175a59560da6408d3632a61cde
Oleksandr Zinchenko <zinchenko@live.com>
2019-08-19 17:50:35 -0700

    Easy to follow implementation of the zigzag conversion

--- leetcode/p0006_zigzag_conversion.py
+++ leetcode/p0006_zigzag_conversion.py
@@ -1,12 +1,13 @@
 from math import ceil
 
+
 def convert(s: str, numRows: int) -> str:
     if numRows == 1:
         return s
 
-    converted_str  = []
+    converted_str = []
     pattern_length = (numRows - 1) * 2
-    s_length       = len(s)
+    s_length = len(s)
     patterns_in_string = ceil(s_length / pattern_length)
 
     for row_ix in range(0, numRows):
@@ -18,9 +19,46 @@ def convert(s: str, numRows: int) -> str:
 
             # the first and the last rows are having one element from the pattern
             # all other intermediate rows are having two elements
-            if row_ix != 0 and row_ix != numRows-1:
-                next_element_ix = pattern_ix * pattern_length + (pattern_length - row_ix)
+            if row_ix != 0 and row_ix != numRows - 1:
+                next_element_ix = pattern_ix * pattern_length + (
+                    pattern_length - row_ix
+                )
                 if next_element_ix >= s_length:
                     break
                 converted_str.append(s[next_element_ix])
-    return ''.join(converted_str)
+    return "".join(converted_str)
+
+
+def convert2(s: str, numRows: int) -> str:
+    """
+    Instead of computing indexes, we can just
+    follow the movement of the string flow.
+    The zigzag pattern makes the string flow
+    up and down that is easy to follow.
+    """
+    if numRows == 1 or numRows > len(s):
+        return s
+
+    # Initially all rows are empty
+    row = [""] * numRows
+
+    # The initial direction is 'down'
+    i, direction = 0, False
+
+    for c in s:
+
+        # append the next character
+        # to the end of current row
+        row[i] += c
+
+        # we have to switch movement direction
+        # as soon as we have reached the begining
+        # or end of rows
+        if i == 0 or i == numRows - 1:
+            direction = not direction
+
+        # move to next (or previous) row
+        # based on the current movement direction
+        i += 1 if direction else -1
+
+    return "".join(row)
--- tests/test_p0006_zigzag_conversion.py
+++ tests/test_p0006_zigzag_conversion.py
@@ -1,13 +1,18 @@
 import pytest
-from leetcode.p0006_zigzag_conversion import convert
+from leetcode.p0006_zigzag_conversion import convert, convert2
 
-@pytest.mark.parametrize(('s', 'numRows', 'expectation'), (
-    ('', 10, ''),
-    ('PAYPALISHIRING', 3, 'PAHNAPLSIIGYIR'),
-    ('PAYPALISHIRING', 4, 'PINALSIGYAHRPI'),
-    ('PAYPALISHIRING', 99, 'PAYPALISHIRING'),
-    ('PAYPALISHIRING', 1, 'PAYPALISHIRING'),
-    ('PAYPALISHIRING', 2, 'PYAIHRNAPLSIIG')
-))
+
+@pytest.mark.parametrize(
+    ("s", "numRows", "expectation"),
+    (
+        ("", 10, ""),
+        ("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"),
+        ("PAYPALISHIRING", 4, "PINALSIGYAHRPI"),
+        ("PAYPALISHIRING", 99, "PAYPALISHIRING"),
+        ("PAYPALISHIRING", 1, "PAYPALISHIRING"),
+        ("PAYPALISHIRING", 2, "PYAIHRNAPLSIIG"),
+    ),
+)
 def test_convert(s, numRows, expectation):
     assert convert(s, numRows) == expectation
+    assert convert2(s, numRows) == expectation
