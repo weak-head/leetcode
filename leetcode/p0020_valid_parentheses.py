c36366adbb74276dc78f08911242706b9058acc7
Oleksandr Zinchenko <zinchenko@live.com>
2019-08-21 08:51:58 -0700

    Another variant of valid pparentheses

--- leetcode/p0020_valid_parentheses.py
+++ leetcode/p0020_valid_parentheses.py
@@ -1,6 +1,6 @@
 def isValid(s: str) -> bool:
-    open_map = {'(': ')', '{': '}', '[': ']'}
-    close_map = {')', '}', ']'}
+    open_map = {"(": ")", "{": "}", "[": "]"}
+    close_map = {")", "}", "]"}
     stack = []
     for char in s:
         if char in open_map:
@@ -12,3 +12,17 @@ def isValid(s: str) -> bool:
             if top != char:
                 return False
     return len(stack) == 0
+
+
+def isValid2(s: str) -> bool:
+    stack = []
+    for c in s:
+        if c in {"(", "{", "["}:
+            stack.append(c)
+        elif c in {")", "}", "]"}:
+            if not stack:
+                return False
+            p = stack.pop()
+            if (p, c) not in {("(", ")"), ("{", "}"), ("[", "]")}:
+                return False
+    return not stack
--- tests/test_p0020_valid_parentheses.py
+++ tests/test_p0020_valid_parentheses.py
@@ -1,15 +1,19 @@
 import pytest
-from leetcode.p0020_valid_parentheses import isValid
+from leetcode.p0020_valid_parentheses import isValid, isValid2
 
 
-@pytest.mark.parametrize(('string', 'valid'), (
-    ('', True),
-    ('()', True),
-    ('(abc + [k] - {as()} + )', True),
-    ('(abc + [k] - as()} + )', False),
-    ('(abc + [k - {as()} + )', False),
-    ('abc + [k] - {as()} + )', False),
-    (')abc + [k] - {as()} + (', False),
-))
+@pytest.mark.parametrize(
+    ("string", "valid"),
+    (
+        ("", True),
+        ("()", True),
+        ("(abc + [k] - {as()} + )", True),
+        ("(abc + [k] - as()} + )", False),
+        ("(abc + [k - {as()} + )", False),
+        ("abc + [k] - {as()} + )", False),
+        (")abc + [k] - {as()} + (", False),
+    ),
+)
 def test_isValid(string, valid):
     assert isValid(string) == valid
+    assert isValid2(string) == valid
