d3d858ab57a4ee26cdd7e680c871dd34bd9652ee
Oleksandr Zinchenko <zinchenko@live.com>
2019-08-23 14:10:36 -0700

    Reverse nodes in k group

--- leetcode/p0025_reverse_nodes_in_k_group.py
+++ leetcode/p0025_reverse_nodes_in_k_group.py
@@ -1,4 +1,3 @@
-
 class ListNode:
     def __init__(self, x):
         self.val = x
@@ -13,10 +12,9 @@ def reverseKGroup(head: ListNode, k: int) -> ListNode:
     new_head.next = head
     ix = 0
 
-    while current != None:
+    while current is not None:
         if ix == k:
-            n_group_head, n_group_end = reverse_group(
-                prev_group_end.next, current)
+            n_group_head, n_group_end = reverse_group(prev_group_end.next, current)
             prev_group_end.next = n_group_head
             prev_group_end = n_group_end
             current = prev_group_end.next
@@ -41,3 +39,73 @@ def reverse_group(head, tail):
 
     last_element.next = next_group_head
     return new_head, last_element
+
+
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+
+
+def reverseKGroup2(head: ListNode, k: int) -> ListNode:
+    """
+    Reverse K-group in the linked list.
+    This implementation is easier to follow, though
+    the complexity is same as above.
+    """
+    if head is None:
+        return None
+
+    prev = dummy = ListNode(None)
+    dummy.next = head
+
+    while True:
+        # Get next group that should be reversed
+        group_head, group_tail = next_group(prev.next, k)
+        if group_tail is None:
+            return dummy.next
+
+        # Reverse the group
+        new_head, new_tail = reverse(group_head, group_tail)
+
+        # Re-attach the new group head and move on
+        prev.next = new_head
+        prev = new_tail
+
+
+def next_group(head, n):
+    """
+    Given the head of the list returns
+    the first and the last element
+    of the next group with length 'n'.
+    """
+    if head is None:
+        return head, None
+
+    curr = head
+    index = 1
+    while index != n and curr:
+        curr = curr.next
+        index += 1
+
+    if index != n:
+        return head, None
+    else:
+        return head, curr
+
+
+def reverse(head, last):
+    """
+    Given the first and any intermediate element in the list
+    reverses the list between them (inclusively).
+    """
+    prev, curr = None, head
+    last_next = last.next
+
+    while prev != last:
+        next = curr.next
+        curr.next = prev
+
+        prev = curr
+        curr = next
+
+    head.next = last_next
+    return prev, head
--- tests/test_p0025_reverse_nodes_in_k_group.py
+++ tests/test_p0025_reverse_nodes_in_k_group.py
@@ -1,6 +1,10 @@
 import pytest
 from typing import List, Generator
-from leetcode.p0025_reverse_nodes_in_k_group import reverseKGroup, ListNode
+from leetcode.p0025_reverse_nodes_in_k_group import (
+    reverseKGroup,
+    reverseKGroup2,
+    ListNode,
+)
 
 
 def from_array(a: List[int]) -> ListNode:
@@ -17,22 +21,24 @@ def to_array(head: ListNode) -> Generator[int, None, None]:
         head = head.next
 
 
-def reverse_convert(a: List[int], k: int) -> List[int]:
+def reverse_convert(a: List[int], k: int, f) -> List[int]:
     head = from_array(a)
-    head = reverseKGroup(head, k)
+    head = f(head, k)
     return list(to_array(head))
 
 
-@pytest.mark.parametrize(('list', 'k', 'result'), (
-    ([], 1, []),
-    ([], 0, []),
-    ([], 110, []),
-
-    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, [1, 2, 3, 4, 5, 6, 7, 8, 9]),
-    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 100, [1, 2, 3, 4, 5, 6, 7, 8, 9]),
-
-    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 2, [ 2, 1, 4, 3, 6, 5, 8, 7, 9]),
-    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, [ 3, 2, 1, 6, 5, 4, 9, 8, 7])
-))
+@pytest.mark.parametrize(
+    ("list", "k", "result"),
+    (
+        ([], 1, []),
+        ([], 0, []),
+        ([], 110, []),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9], 100, [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9], 2, [2, 1, 4, 3, 6, 5, 8, 7, 9]),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, [3, 2, 1, 6, 5, 4, 9, 8, 7]),
+    ),
+)
 def test_reverse_nodes(list, k, result):
-    reverse_convert(list, k) == result
+    reverse_convert(list, k, reverseKGroup) == result
+    reverse_convert(list, k, reverseKGroup2) == result
