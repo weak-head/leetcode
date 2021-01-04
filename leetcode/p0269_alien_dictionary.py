a37b928f32844b6ffefc5b4d551296e49befa61c
Oleksandr Zinchenko <zinchenko@live.com>
2021-01-03 18:31:28 -0800

    269 - Alien Dictionary (DFS)

--- leetcode/p0269_alien_dictionary.py
+++ leetcode/p0269_alien_dictionary.py
@@ -5,6 +5,8 @@ from collections import defaultdict, Counter, deque
 def alienOrderKahn(words: List[str]) -> str:
     """
     Based on Kahn's algorithm
+    O(c)
+    c - total length of all words combined
     """
     adj_list = defaultdict(set)
     in_degree = Counter({c: 0 for word in words for c in word})
@@ -32,6 +34,45 @@ def alienOrderKahn(words: List[str]) -> str:
                 no_incoming_edges.append(to_node)
 
     if len(output) < len(in_degree):
+        return ""
+
+    return "".join(output)
+
+
+def alienOrderDFS(words: List[str]) -> str:
+    """
+    Topological sort, DFS
+    O(c)
+    c - total length of all words combined
+    """
+    reverse_adj_list = {c: set() for word in words for c in word}
+
+    for wordA, wordB in zip(words, words[1:]):
+        for chA, chB in zip(wordA, wordB):
+            if chA != chB:
+                reverse_adj_list[chB].add(chA)
+                break
+        else:
+            if len(wordB) < len(wordA):
+                return ""
+
+    seen = {}  # NotIn = white, False = grey, True = black
+    output = []
+
+    def visit(node):
+        if node in seen:
+            return seen[node]
+
+        seen[node] = False  # grey
+        for next_node in reverse_adj_list[node]:
+            result = visit(next_node)
+            if not result:  # was grey => cycle
+                return False
+        seen[node] = True  # black
+        output.append(node)
+        return True
+
+    if not all(visit(node) for node in reverse_adj_list):
         return ""
 
     return "".join(output)
--- tests/test_p0269_alien_dictionary.py
+++ tests/test_p0269_alien_dictionary.py
@@ -1,15 +1,16 @@
 import pytest
-from leetcode.p0269_alien_dictionary import alienOrderKahn
+from leetcode.p0269_alien_dictionary import alienOrderKahn, alienOrderDFS
 
 
 @pytest.mark.parametrize(
     ("a", "expectation"),
     (
-        (["wrt", "wrf", "er", "ett", "rftt"], "wertf"),
-        (["za", "zb", "ca", "cb"], "zacb"),
-        (["aab", "aabb", "bb"], "ab"),
-        (["aabb", "aab", "bb"], ""),
+        (["wrt", "wrf", "er", "ett", "rftt"], {"wertf"}),
+        (["za", "zb", "ca", "cb"], {"zacb", "zabc"}),
+        (["aab", "aabb", "bb"], {"ab"}),
+        (["aabb", "aab", "bb"], {""}),
     ),
 )
 def test_alienOrder(a, expectation):
-    assert alienOrderKahn(a) == expectation
+    assert alienOrderKahn(a) in expectation
+    assert alienOrderDFS(a) in expectation
