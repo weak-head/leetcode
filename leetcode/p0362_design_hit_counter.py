b84b7cbe0332f88f55752f01d5d6176dbe2d9688
Oleksandr Zinchenko <zinchenko@live.com>
2021-01-04 10:40:37 -0800

    362 - Hit Counter (Queue and Queue with pairs)

--- leetcode/p0362_design_hit_counter.py
+++ leetcode/p0362_design_hit_counter.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class HitCounterBuckets:
     def __init__(self, seconds):
         self._seconds = seconds
@@ -25,3 +28,54 @@ class HitCounterBuckets:
             if timestamp - time < self._seconds:
                 hits += hit
         return hits
+
+
+class HitCounterQueue:
+    def __init__(self, seconds):
+        self._seconds = seconds
+        self.queue = deque()
+
+    def hit(self, timestamp: int) -> None:
+        """
+        O(1)
+        """
+        self.queue.appendleft(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        """
+        O(n)
+        """
+        while self.queue and timestamp - self.queue[-1] >= self._seconds:
+            self.queue.pop()
+        return len(self.queue)
+
+
+class HitCounterQueuePair:
+    def __init__(self, seconds):
+        self._seconds = seconds
+        self.q = deque()  # (time, hits)
+        self.total = 0
+
+    def hit(self, timestamp: int) -> None:
+        """
+        O(1)
+        """
+        if self.q and self.q[-1][0] == timestamp:
+            self.q[-1] = self.q[-1][0], self.q[-1][1] + 1
+        else:
+            self.q.append((timestamp, 1))
+        self.total += 1
+
+    def getHits(self, timestamp: int) -> int:
+        """
+        O(n) - worst case
+        O(1) - amortized, in case of lots of concurrent calls at the same timestamp
+        """
+        while self.q:
+            diff = timestamp - self.q[0][0]
+            if diff >= self._seconds:
+                _, hit = self.q.popleft()
+                self.total -= hit
+            else:
+                break
+        return self.total
--- tests/test_p0362_design_hit_counter.py
+++ tests/test_p0362_design_hit_counter.py
@@ -1,15 +1,22 @@
-from leetcode.p0362_design_hit_counter import HitCounterBuckets
+from leetcode.p0362_design_hit_counter import (
+    HitCounterBuckets,
+    HitCounterQueue,
+    HitCounterQueuePair,
+)
 
 
 def test_HitCounter():
-    hcs = [HitCounterBuckets(300)]
+    hcs = [HitCounterBuckets(300), HitCounterQueue(300), HitCounterQueuePair(300)]
 
     for hc in hcs:
         hc.hit(1)
         hc.hit(2)
-        hc.hit(3)
-        assert hc.getHits(4) == 3
+        hc.hit(2)
+        hc.hit(2)
+        for _ in range(500):
+            hc.hit(3)
+        assert hc.getHits(4) == 504
 
         hc.hit(300)
-        assert hc.getHits(300) == 4
-        assert hc.getHits(301) == 3
+        assert hc.getHits(300) == 505
+        assert hc.getHits(301) == 504
