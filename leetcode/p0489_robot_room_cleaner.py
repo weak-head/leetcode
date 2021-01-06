063898638955f95ccdde0131da761976671de785
Oleksandr Zinchenko <zinchenko@live.com>
2021-01-06 12:52:04 -0800

    489 - Robot Room Cleaner

---
+++ leetcode/p0489_robot_room_cleaner.py
@@ -0,0 +1,75 @@
+class Robot:
+    def move(self):
+        """
+       Returns true if the cell in front is open and robot moves into the cell.
+       Returns false if the cell in front is blocked and robot stays in the current cell.
+       :rtype bool
+       """
+        pass
+
+    def turnLeft(self):
+        """
+       Robot will stay in the same cell after calling turnLeft/turnRight.
+       Each turn will be 90 degrees.
+       :rtype void
+       """
+        pass
+
+    def turnRight(self):
+        """
+       Robot will stay in the same cell after calling turnLeft/turnRight.
+       Each turn will be 90 degrees.
+       :rtype void
+       """
+        pass
+
+    def clean(self):
+        """
+       Clean the current cell.
+       :rtype void
+       """
+        pass
+
+
+def cleanRoom(robot: "Robot"):
+    """
+    O(n - m)
+    n - number of cells in the room
+    m - number of obstacles
+    """
+    # visited cells
+    visited = set()
+    # up, right, down, left
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+    def go_back():
+        robot.turnRight()
+        robot.turnRight()
+        robot.move()
+        robot.turnRight()
+        robot.turnRight()
+
+    def backtrack(cell=(0, 0), d=0):
+        visited.add(cell)
+        robot.clean()
+
+        for i in range(4):
+            # using the base direction 'd' to
+            # evaluate the new cell we need to move to
+            new_direction = (d + i) % 4
+
+            # The next cell we will be at
+            # when we make a 'move'
+            new_cell = (
+                cell[0] + directions[new_direction][0],
+                cell[1] + directions[new_direction][1],
+            )
+
+            if new_cell not in visited and robot.move():
+                backtrack(new_cell, new_direction)
+                go_back()
+
+            # All our directions turn are right-handed
+            robot.turnRight()
+
+    backtrack()
---
+++ tests/test_p0489_robot_room_cleaner.py
@@ -0,0 +1,6 @@
+# from leetcode.p0489_robot_room_cleaner import Robot, cleanRoom
+
+
+def test_Robot(a, expectation):
+    # out of scope
+    assert 1 == 1
