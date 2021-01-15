cab03db8af9da4c6c13c49a2bf64d527290f8b72
Oleksandr Zinchenko <zinchenko@live.com>
2021-01-14 19:14:30 -0800

    339 - Nested List Weight Sum

---
+++ leetcode/p0339_nested_list_weight_sum.py
@@ -0,0 +1,62 @@
+from typing import List
+
+
+class NestedInteger:
+    def __init__(self, value=None):
+        """
+        If value is not specified, initializes an empty list.
+        Otherwise initializes a single integer equal to value.
+        """
+        pass
+
+    def isInteger(self):
+        """
+        @return True if this NestedInteger holds a single integer, rather than a nested list.
+        :rtype bool
+        """
+        pass
+
+    def add(self, elem):
+        """
+        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+        :rtype void
+        """
+        pass
+
+    def setInteger(self, value):
+        """
+        Set this NestedInteger to hold a single integer equal to value.
+        :rtype void
+        """
+        pass
+
+    def getInteger(self):
+        """
+        @return the single integer that this NestedInteger holds, if it holds a single integer
+        Return None if this NestedInteger holds a nested list
+        :rtype int
+        """
+        pass
+
+    def getList(self):
+        """
+        @return the nested list that this NestedInteger holds, if it holds a nested list
+        Return None if this NestedInteger holds a single integer
+        :rtype List[NestedInteger]
+        """
+        pass
+
+
+def depthSum(self, nestedList: List[NestedInteger]) -> int:
+    """
+    O(n)
+    n - total number of integer elements in all lists combined
+    """
+
+    def mul(ni, d):
+        if ni.isInteger():
+            return ni.getInteger() * d
+        else:
+            return sum([mul(s, d + 1) for s in ni.getList()])
+
+    return sum([mul(s, 1) for s in nestedList])
