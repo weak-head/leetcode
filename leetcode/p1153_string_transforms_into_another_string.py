897ef659e98ef687841f270e3933ae924e8f3333
Oleksandr Zinchenko <zinchenko@live.com>
2021-01-27 15:00:57 -0800

    1153 - String Transforms Into Another String
    Fixed comments

--- leetcode/p1153_string_transforms_into_another_string.py
+++ leetcode/p1153_string_transforms_into_another_string.py
@@ -1,14 +1,19 @@
 def canConvert(str1: str, str2: str) -> bool:
+    """
+    Time: O(n)
+    Space: O(n)
+        n - number of characters in the string
+    """
     if str1 == str2:
         return True
 
     conversions = {}
-    # strings are of the same length
     for c1, c2 in zip(str1, str2):
-        # we can have only 1 to 1 mapping,
+        # We can have only 1 to 1 mapping,
         # otherwise we can't convert
         if conversions.setdefault(c1, c2) != c2:
             return False
 
-    # We can use only 26 lowercase English characters
+    # We need to have 1 unused character in 'str2',
+    # otherwise we can't covert
     return len(set(str2)) < 26
