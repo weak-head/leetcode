7ce48784465308e54025dbbb453421d513c35d67
Oleksandr Zinchenko <zinchenko@live.com>
2021-02-01 17:56:03 -0800

    4 - Median Of Two Sorted Arrays

--- leetcode/p0004_median_of_two_sorted_arrays.py
+++ leetcode/p0004_median_of_two_sorted_arrays.py
@@ -1,52 +1,45 @@
 from typing import List
 
 
-def findMedianSortedArrays(a: List[int], b: List[int]) -> float:
-    a_len, b_len = len(a), len(b)
-    kth_index = (a_len + b_len) >> 1
-    if (a_len + b_len) % 2:
-        return kth_elem(a, b, 0, a_len - 1, 0, b_len - 1, kth_index)
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    """
+    Find median using find k-th element.
+
+    Time: O(log(min(n, m)))
+        n - length of first array
+        m - length of second array
+
+    Space: O(1)
+    """
+    n1, n2 = len(nums1), len(nums2)
+    k = (n1 + n2) >> 1
+
+    if (n1 + n2) % 2 == 0:
+        v1 = kth(nums1, nums2, 0, n1 - 1, 0, n2 - 1, k - 1)
+        v2 = kth(nums1, nums2, 0, n1 - 1, 0, n2 - 1, k)
+        return (v1 + v2) / 2
     else:
-        kth_1 = kth_elem(a, b, 0, a_len - 1, 0, b_len - 1, kth_index - 1)
-        kth_2 = kth_elem(a, b, 0, a_len - 1, 0, b_len - 1, kth_index)
-        return (kth_1 + kth_2) / 2
+        return kth(nums1, nums2, 0, n1 - 1, 0, n2 - 1, k)
 
 
-def kth_elem(
-    a: List[int],
-    b: List[int],
-    a_l_ix: int,
-    a_r_ix: int,
-    b_l_ix: int,
-    b_r_ix: int,
-    kth_index: int,
-) -> int:
-    if a_l_ix > a_r_ix:
-        return b[kth_index - a_l_ix]
-    if b_l_ix > b_r_ix:
-        return a[kth_index - b_l_ix]
+def kth(a, b, al, ar, bl, br, k):
+    """
+    Find k-th element of two sorted arrays.
+    """
+    if al > ar:
+        return b[k - al]
+    if bl > br:
+        return a[k - bl]
 
-    a_m_ix = (a_l_ix + a_r_ix) >> 1
-    b_m_ix = (b_l_ix + b_r_ix) >> 1
-    a_m, b_m = a[a_m_ix], b[b_m_ix]
-
-    # we are still behind the median index, we need to move forward
-    if a_m_ix + b_m_ix < kth_index:
-        # a_m comes before b_m
-        # we need to move a_l_ix further right
-        if a_m < b_m:
-            return kth_elem(a, b, a_m_ix + 1, a_r_ix, b_l_ix, b_r_ix, kth_index)
-        # b_m comes before a_m
-        # we need to move it right
+    am = (al + ar) >> 1
+    bm = (bl + br) >> 1
+    if am + bm < k:  # need to increase the total index
+        if a[am] < b[bm]:
+            return kth(a, b, am + 1, ar, bl, br, k)
         else:
-            return kth_elem(a, b, a_l_ix, a_r_ix, b_m_ix + 1, b_r_ix, kth_index)
-    # we have passed the kth_index and we need to move backward
-    else:
-        # a_m comes after b_m
-        # we need to move a_r_ix backward
-        if a_m > b_m:
-            return kth_elem(a, b, a_l_ix, a_m_ix - 1, b_l_ix, b_r_ix, kth_index)
-        # b_m comes after a_m
-        # we need
+            return kth(a, b, al, ar, bm + 1, br, k)
+    else:  # need to reduce the total index
+        if a[am] > b[bm]:
+            return kth(a, b, al, am - 1, bl, br, k)
         else:
-            return kth_elem(a, b, a_l_ix, a_r_ix, b_l_ix, b_m_ix - 1, kth_index)
+            return kth(a, b, al, ar, bl, bm - 1, k)
