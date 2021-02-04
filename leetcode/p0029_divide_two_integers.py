70e00447e01dd13e54922226dbe7c87671e0510f
Oleksandr Zinchenko <zinchenko@live.com>
2021-02-04 10:22:14 -0800

    29 - Divide Two Integers

--- leetcode/p0029_divide_two_integers.py
+++ leetcode/p0029_divide_two_integers.py
@@ -1,18 +1,21 @@
 def divide(dividend: int, divisor: int) -> int:
     """
     The fast division that subtracts powers of divisor from dividend.
+
+    Time: O(log n)
+    Space: O(1)
+        n - number of bits in dividend
     """
     sign = -1 if (divisor < 0 and dividend > 0) or (dividend < 0 and divisor > 0) else 1
     dividend, divisor = abs(dividend), abs(divisor)
-    result = 0
+    quotient = 0
 
     while dividend >= divisor:
-        acc_div, tr = divisor, 1
-        while dividend >= acc_div:
-            tr = tr << 1
-            acc_div = acc_div << 1
-        dividend -= acc_div >> 1
-        result += tr >> 1
+        d, t = divisor, 1
+        while dividend >= d:
+            t, d = t << 1, d << 1
+        dividend -= d >> 1
+        quotient += t >> 1
 
-    result = result * sign
-    return min((2 ** 31 - 1), max(-(2 ** 31), result))
+    quotient = quotient * sign
+    return min((2 ** 31 - 1), max(-(2 ** 31), quotient))
