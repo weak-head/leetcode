9e4cdedaf45262a171ba64e20fa8d4b83d556ad3
Oleksandr Zinchenko <zinchenko@live.com>
2021-02-05 12:37:51 -0800

    0037 - sudoku solver

--- leetcode/p0037_sudoku_solver.py
+++ leetcode/p0037_sudoku_solver.py
@@ -1,40 +1,44 @@
 from typing import List
 
 
-def solveSudoku(board: List[List[str]]):
-    return backtrack(board, 0, 0)
+def solveSudoku(board: List[List[str]]) -> None:
+    """
+    Time: O(9! ^ 9) = O(1)
+    Space: O(9 * 9) = O(1)
+    """
 
+    def candidates(row, col):
+        ca = set("123456789")
+        for i in range(9):
+            if board[i][col] in ca:
+                ca.remove(board[i][col])
+            if board[row][i] in ca:
+                ca.remove(board[row][i])
 
-def backtrack(board, row, column):
-    if row >= 9:
-        return True
+        br, bc = (row // 3) * 3, (col // 3) * 3
+        for r in range(br, br + 3):
+            for c in range(bc, bc + 3):
+                if board[r][c] in ca:
+                    ca.remove(board[r][c])
+        return ca
 
-    if column >= 9:
-        return backtrack(board, row + 1, 0)
+    def backtrack(row, col):
+        if row > 8:
+            return True
 
-    if board[row][column] == ".":
-        for num in possible_nums(board, row, column):
-            board[row][column] = num
-            if backtrack(board, row, column + 1):
-                return True
-            else:
-                board[row][column] = "."
-    else:
-        return backtrack(board, row, column + 1)
+        if col > 8:
+            return backtrack(row + 1, 0)
 
-    return False
+        if board[row][col] != ".":
+            return backtrack(row, col + 1)
+        else:
+            v = board[row][col]
+            for candidate in candidates(row, col):
+                board[row][col] = candidate
+                if backtrack(row, col + 1):
+                    return True
+            board[row][col] = v
 
+        return False
 
-def possible_nums(board, row, column):
-    all = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-    filled = set(board[row])
-
-    for r in range(9):
-        filled.add(board[r][column])
-
-    r_rect, c_rect = (row // 3) * 3, (column // 3) * 3
-    for rix in range(r_rect, r_rect + 3):
-        for cix in range(c_rect, c_rect + 3):
-            filled.add(board[rix][cix])
-
-    return list(all - filled)
+    return backtrack(0, 0)
