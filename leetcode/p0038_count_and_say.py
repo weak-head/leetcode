73a63ca66fdf8b3b3f1d4a011e1258186c0ac1b4
Oleksandr Zinchenko <zinchenko@live.com>
2021-02-05 15:17:21 -0800

    0038 - count and_say

--- leetcode/p0038_count_and_say.py
+++ leetcode/p0038_count_and_say.py
@@ -1,34 +1,21 @@
-s_to_n = {"a": "1", "b": "2", "c": "3"}
-n_to_s = {1: "a", 2: "b", 3: "c"}
-
-
 def countAndSay(n: int) -> str:
+    """
+    Time: (2^n)
+    Space: (2^n)
+    """
+
     r = "1"
     for _ in range(n - 1):
-        r = count(say(r))
+
+        t, c, k = [], r[0], 0
+        for v in r:
+            if v == c:
+                k += 1
+            else:
+                t.extend([k, c])
+                c, k = v, 1
+
+        t.extend([k, c])
+        r = "".join(map(str, t))
+
     return r
-
-
-def count(s: str) -> str:
-    r = []
-    for n in s:
-        if n in s_to_n:
-            r.append(s_to_n[n])
-        else:
-            r.append(n)
-    return "".join(r)
-
-
-def say(n: str) -> str:
-    r = []
-    c, k = n[0], 0
-    for v in n:
-        if v == c:
-            k = k + 1
-        else:
-            r.append(n_to_s[k])
-            r.append(c)
-            c, k = v, 1
-    r.append(n_to_s[k])
-    r.append(c)
-    return "".join(r)
