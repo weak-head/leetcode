81730b7e86109ac0caddc00cb18bc587a0729536
Oleksandr Zinchenko <zinchenko@live.com>
2021-02-06 15:14:55 -0800

    0040 - combination sum ii

--- leetcode/p0040_combination_sum_ii.py
+++ leetcode/p0040_combination_sum_ii.py
@@ -2,26 +2,34 @@ from typing import List
 
 
 def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
-    result = []
-    backtrack([], target, 0, sorted(candidates), result)
-    return result
+    """
+    Time: O(2^n) # all subsets of candidates set
+    Space: O(n)  # all candidates used
+        n - number of candidates
+    """
 
+    def backtrack(candidates, combinations, combination, index, target):
+        if target < 0:
+            return
 
-def backtrack(seq, target, index, candidates, result):
-    if target < 0:
-        return
+        if target == 0:
+            combinations.append(combination)
+            return
 
-    if target == 0:
-        result.append(seq)
-        return
+        for i in range(index, len(candidates)):
+            num = candidates[i]
 
-    for i in range(index, len(candidates)):
-        num = candidates[i]
+            # would yeld the same combination as before
+            if i > index and candidates[i - 1] == num:
+                continue
 
-        if i > index and candidates[i - 1] == candidates[i]:
-            continue
+            if num > target:
+                break
 
-        if num > target:
-            break
+            backtrack(
+                candidates, combinations, combination + [num], i + 1, target - num
+            )
 
-        backtrack(seq + [num], target - num, i + 1, candidates, result)
+    combinations = []
+    backtrack(sorted(candidates), combinations, [], 0, target)
+    return combinations
