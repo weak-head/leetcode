8e84a2f793f86aa02529a0feb58d6da3b5abef41
Oleksandr Zinchenko <zinchenko@live.com>
2021-02-06 12:33:22 -0800

    exclusive time of processes

--- problems/x_exclusive_time_of_processes.py
+++ problems/x_exclusive_time_of_processes.py
@@ -20,59 +20,35 @@
 
 
 from collections import defaultdict
-from functools import cmp_to_key
 
 
 def exclusive_time(procs):
     """
-    This problem could be solved using sweep line algorithm.
-    Another way is priority queue.
+    Sweep line.
+
+    Time: O(n * log n)
+    Space: O(n)
+        n - number of elements in the list
     """
-
-    # split time series of processes into separate sorted events:
-    # (id, kind, time)
-    id, kind, time = 0, 1, 2
     events = []
-    for proc in procs:
-        events.append((proc[0], "start", proc[1]))
-        events.append((proc[0], "end", proc[2]))
+    for proc, start, end in procs:
+        events.append((start, 1, proc))
+        events.append((end, 0, proc))
+    events.sort()  # end goes before start
 
-    # order events based on time and event kind,
-    # if time is same 'end' should come before 'start'
-    def comparator(a, b):
-        # if same time
-        if a[time] == b[time]:
-            return 1 if a[kind] == "start" else -1
-        return -1 if a[time] < b[time] else 1
+    active_time = defaultdict(int)
+    active = set()
+    last_time = None
+    for time, kind, proc in events:
+        if kind == 0:  # end
+            if len(active) == 1:
+                active_time[proc] += time - last_time
+            active.remove(proc)
+        else:  # start
+            if len(active) == 1:
+                p = list(active)[0]
+                active_time[p] += time - last_time
+            active.add(proc)
+        last_time = time
 
-    events = sorted(events, key=cmp_to_key(comparator))
-
-    results = defaultdict(int)
-    active_procs = set()
-    last_time = events[0][time]  # start time of the entire series
-
-    for event in events:
-
-        if event[kind] == "start":
-
-            # the process is not single any more
-            # we need to update it's singleton running time
-            if len(active_procs) == 1:
-                single = list(active_procs)[0]
-                results[single] += event[time] - last_time
-
-            active_procs.add(event[id])
-
-        else:
-
-            # the active process has finished
-            if len(active_procs) == 1:
-                single = list(active_procs)[0]
-                results[single] += event[time] - last_time
-
-            active_procs.remove(event[id])
-
-        last_time = event[time]
-
-    # ignoring zero time processes
-    return sorted([(k, v) for k, v in results.items() if v != 0])
+    return sorted([(k, v) for k, v in active_time.items() if v != 0])
