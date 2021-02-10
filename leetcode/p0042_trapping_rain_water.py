52a4f3bc49b66b9fbab829e98d5b85c344703937
Oleksandr Zinchenko <zinchenko@live.com>
2021-02-09 17:21:57 -0800

    0042 - trapping rain water

--- leetcode/p0042_trapping_rain_water.py
+++ leetcode/p0042_trapping_rain_water.py
@@ -2,6 +2,45 @@ from typing import List
 
 
 def trap(height: List[int]) -> int:
+    """
+    Two pointers, greedy
+
+    As long as right_max[i] > left_max[i]
+    water trapped depends on left_max.
+    And similar in case if left_max[i] > right_max[i]
+    watter trapped depends on right_max.
+
+    Time: O(n)
+    Space: O(1)
+        n - number of elements in the array
+    """
+    l, r = 0, len(height) - 1
+    l_max, r_max = 0, 0
+    volume = 0
+
+    while l < r:
+        if height[l] < height[r]:
+            if height[l] >= l_max:
+                l_max = height[l]
+            else:
+                volume += l_max - height[l]
+            l += 1
+        else:
+            if height[r] >= r_max:
+                r_max = height[r]
+            else:
+                volume += r_max - height[r]
+            r -= 1
+
+    return volume
+
+
+def trap2(height: List[int]) -> int:
+    """
+    Time: O(n)
+    Space: O(n)
+        n - number of elements in the array
+    """
     res, res_len = list(height), len(height)
 
     max_v = 0
--- tests/test_p0042_trapping_rain_water.py
+++ tests/test_p0042_trapping_rain_water.py
@@ -1,5 +1,10 @@
 import pytest
-from leetcode.p0042_trapping_rain_water import trap
+from leetcode.p0042_trapping_rain_water import trap, trap2
+
+solutions = [
+    trap,
+    trap2,
+]
 
 
 @pytest.mark.parametrize(
@@ -12,7 +17,10 @@ from leetcode.p0042_trapping_rain_water import trap
         ([100, 0, 0, 0, 0, 0], 0),
         ([0, 0, 0, 0, 0, 100], 0),
         ([1, 0, 0, 0, 0, 0, 100], 5),
+        ([0, 1, 2, 3, 4, 5, 0, 5, 4, 3, 2, 1], 5),
+        ([0, 1, 2, 3, 4, 5, 0, 0, 0, 0], 0),
     ),
 )
-def test_trap(a, expectation):
-    assert trap(a) == expectation
+@pytest.mark.parametrize(("solution"), solutions)
+def test_trap(a, expectation, solution):
+    assert solution(a) == expectation
