bfc47c8c64496049100a906190c8be2d9acaf0ca
Oleksandr Zinchenko <zinchenko@live.com>
2021-02-11 05:43:03 -0800

    0387 - first unique character in a string

--- leetcode/p0387_first_unique_character_in_a_string.py
+++ leetcode/p0387_first_unique_character_in_a_string.py
@@ -1,29 +1,16 @@
-class Node:
-    def __init__(self, val, ix, next, prev):
-        self.val = val
-        self.ix = ix
-        self.prev = prev
-        self.next = next
+from collections import Counter
 
 
 def firstUniqChar(s: str) -> int:
-    chars = {}
-    head = Node(None, None, None, None)
-    head.next = head.prev = head
+    """
+    Time: O(n)
+    Space: O(n)
+        n - length of the string
+    """
 
-    for ix, c in enumerate(s):
-        if c not in chars:
-            node = Node(c, ix, head, head.prev)
-            head.prev.next = node
-            head.prev = node
-            chars[c] = node
-        else:
-            node = chars[c]
-            if node is not None:
-                node.prev.next = node.next
-                if node.next is not None:
-                    node.next.prev = node.prev
-                chars[c] = None
+    cnt = Counter(s)
+    for i, c in enumerate(s):
+        if cnt[c] == 1:
+            return i
 
-    result = head.next
-    return result.ix if result.val else -1
+    return -1
