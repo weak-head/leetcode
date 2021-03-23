a019bc2153ab02f765aa903a0c90c67a0b2191f5
Oleksandr Zinchenko <zinchenko@live.com>
2021-03-23 14:51:43 -0700

    0043 - multiply strings

--- leetcode/p0043_multiply_strings.py
+++ leetcode/p0043_multiply_strings.py
@@ -1,145 +1,29 @@
-nums = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
-adds = {
-    0: ("0", 0),
-    1: ("1", 0),
-    2: ("2", 0),
-    3: ("3", 0),
-    4: ("4", 0),
-    5: ("5", 0),
-    6: ("6", 0),
-    7: ("7", 0),
-    8: ("8", 0),
-    9: ("9", 0),
-    10: ("0", 1),
-    11: ("1", 1),
-    12: ("2", 1),
-    13: ("3", 1),
-    14: ("4", 1),
-    15: ("5", 1),
-    16: ("6", 1),
-    17: ("7", 1),
-    18: ("8", 1),
-    19: ("9", 1),
-}
-
-
 def multiply(num1: str, num2: str) -> str:
-    if num1 == "0" or num2 == "0":
-        return "0"
+    """
+    Generalized primary school multiplication.
 
-    muls = build_muls(num1)
-    d, result = 0, ["0"]
-    for digit in reversed(num2):
-        # multiplication result for this digit
-        res = muls[digit].copy()
+    Time: O(n * m)
+    Space: O(n + m)
+        n - length of the first number
+        m - length of the second number
+    """
+    r = [0] * (len(num1) + len(num2))
 
-        # append the necessary number of zeroes
-        # to reflect the digit power
-        for _ in range(d):
-            res.append("0")
+    for i in range(len(num1) - 1, -1, -1):
+        for j in range(len(num2) - 1, -1, -1):
+            mul = int(num1[i]) * int(num2[j])
+            minor = i + j + 1
+            major = i + j
+            res_sum = r[minor] + mul
+            r[minor] = res_sum % 10
+            r[major] += res_sum // 10
 
-        # sum the multiplication result
-        result = add(res, result)
-        print(num1, " * ", digit, " = ", "".join(res), "total: ", "".join(result))
+    # skip leading zeros
+    res, i = [], 0
+    while i < len(r) and r[i] == 0:
+        i += 1
 
-        # next digit
-        d = d + 1
+    for j in range(i, len(r)):
+        res.append(str(r[j]))
 
-    return "".join(result)
-
-
-def build_muls(num: str) -> dict:
-    muls = {"0": ["0"]}
-    muls["1"] = add(num, muls["0"])
-    muls["2"] = add(num, num)
-    muls["3"] = add(num, muls["2"])
-    muls["4"] = add(muls["2"], muls["2"])
-    muls["5"] = add(muls["3"], muls["2"])
-    muls["6"] = add(muls["3"], muls["3"])
-    muls["7"] = add(muls["4"], muls["3"])
-    muls["8"] = add(muls["4"], muls["4"])
-    muls["9"] = add(muls["5"], muls["4"])
-    return muls
-
-
-def add(num1, num2):
-    res, carry = [], 0
-
-    num1_len, num2_len = len(num1), len(num2)
-    for i in range(max(num1_len, num2_len)):
-        ix1 = num1_len - i - 1
-        ix2 = num2_len - i - 1
-        v1 = num1[ix1] if 0 <= ix1 < num1_len else "0"
-        v2 = num2[ix2] if 0 <= ix2 < num2_len else "0"
-
-        r, carry = addc(v1, v2, carry)
-        res.append(r)
-
-    if carry:
-        res.append("1")
-
-    res.reverse()
-    return res
-
-
-def addc(d1: str, d2: str, carry: int) -> (str, int):
-    n1, n2 = nums[d1], nums[d2]
-    res = n1 + n2 + carry
-    return adds[res]
-
-
-# ---------------------------
-
-
-def multiply2(num1: str, num2: str) -> str:
-    def to_int(c: str) -> int:
-        return ord(c) - ord("0")
-
-    def from_int(c: int) -> str:
-        return chr(ord("0") + c)
-
-    num1_len, num2_len = len(num1), len(num2)
-    res = [0] * (num1_len + num2_len)
-
-    for ix1 in range(num1_len - 1, -1, -1):
-        for ix2 in range(num2_len - 1, -1, -1):
-            mul = to_int(num1[ix1]) * to_int(num2[ix2])
-
-            rix1 = ix1 + ix2
-            rix2 = rix1 + 1
-
-            mul = mul + res[rix2]
-            res[rix1] = res[rix1] + (mul // 10)
-            res[rix2] = mul % 10
-
-    sres = []
-    for v in res:
-        if not (len(sres) == 0 and v == 0):
-            sres.append(from_int(v))
-
-    return "".join(sres) if sres != [] else "0"
-
-
-# ---------------------------
-
-
-def multiply3(num1: str, num2: str) -> str:
-    def to_int(s: str) -> int:
-        d, res = 1, 0
-        for v in range(len(s) - 1, -1, -1):
-            c = ord(s[v]) - ord("0")
-            res = res + (c * d)
-            d = d * 10
-        return res
-
-    def to_str(n: int) -> str:
-        res = []
-        while n != 0:
-            n, v = divmod(n, 10)
-            res.append(chr(ord("0") + v))
-        return "".join(reversed(res)) if res != [] else "0"
-
-    n1 = to_int(num1)
-    n2 = to_int(num2)
-    r = n1 * n2
-    return to_str(r)
+    return "0" if not res else "".join(res)
--- tests/test_p0043_multiply_strings.py
+++ tests/test_p0043_multiply_strings.py
@@ -1,11 +1,5 @@
 import pytest
-from leetcode.p0043_multiply_strings import (
-    multiply,
-    multiply2,
-    multiply3,
-    add,
-    build_muls,
-)
+from leetcode.p0043_multiply_strings import multiply
 
 
 @pytest.mark.parametrize(
@@ -19,50 +13,14 @@ from leetcode.p0043_multiply_strings import (
         ("592", "943", "558256"),
         ("1234567890", "987654321", "1219326311126352690"),
         ("5938238512", "392839592090301935", "2332775194789001532125120720"),
+        (
+            "2934293479237492783492374927345938238512",
+            "723894729347392839592090301935",
+            "2124119583978444509094535306984539867031889677545823075404432125120720",
+        ),
+        ("0", "392839592090301935", "0"),
+        ("5938238512", "0", "0"),
     ),
 )
 def test_multipy(a, b, expectation):
     assert multiply(a, b) == expectation
-    assert multiply2(a, b) == expectation
-    assert multiply3(a, b) == expectation
-
-
-@pytest.mark.parametrize(
-    ("a", "b", "result"),
-    (
-        ("123", "0", ["1", "2", "3"]),
-        ("0", "123", ["1", "2", "3"]),
-        ("999", "1", ["1", "0", "0", "0"]),
-        ("998", "1", ["9", "9", "9"]),
-        ("0", "0", ["0"]),
-        ("12345", "67890", ["8", "0", "2", "3", "5"]),
-        ("999", "1001", ["2", "0", "0", "0"]),
-        ("999", "11001", ["1", "2", "0", "0", "0"]),
-    ),
-)
-def test_add(a, b, result):
-    assert add(a, b) == result
-
-
-@pytest.mark.parametrize(
-    ("a", "muls"),
-    (
-        (
-            "11",
-            {
-                "0": "0",
-                "1": "11",
-                "2": "22",
-                "3": "33",
-                "4": "44",
-                "5": "55",
-                "6": "66",
-                "7": "77",
-                "8": "88",
-                "9": "99",
-            },
-        ),
-    ),
-)
-def test_muls(a, muls):
-    build_muls(a) == muls
