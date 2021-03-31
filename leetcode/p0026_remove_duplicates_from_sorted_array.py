0e50e45dab756a09c321d60228f30d4f191dc146
Oleksandr Zinchenko <zinchenko@live.com>
2021-03-30 20:04:24 -0700

    0026 - remove duplicates from sorted array

--- leetcode/p0026_remove_duplicates_from_sorted_array.py
+++ leetcode/p0026_remove_duplicates_from_sorted_array.py
@@ -2,11 +2,17 @@ from typing import List
 
 
 def removeDuplicates(nums: List[int]) -> int:
-    if nums == []:
+    """
+    Time: O(n)
+    Space: O(1)
+    """
+    if not nums:
         return 0
-    u_ix = 0
-    for c_ix in range(len(nums)):
-        if nums[u_ix] != nums[c_ix]:
-            u_ix = u_ix + 1
-            nums[u_ix] = nums[c_ix]
-    return u_ix + 1
+
+    l = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[l] = nums[i]
+            l += 1
+
+    return l
