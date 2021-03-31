9e88f71944f296a42c651dfb96f94e0c0c11a079
Oleksandr Zinchenko <zinchenko@live.com>
2021-03-30 19:24:19 -0700

    0042 - trapping rain water

--- leetcode/p0042_trapping_rain_water.py
+++ leetcode/p0042_trapping_rain_water.py
@@ -14,22 +14,22 @@ def trap(height: List[int]) -> int:
     Space: O(1)
         n - number of elements in the array
     """
+    if not height:
+        return 0
+
     l, r = 0, len(height) - 1
-    l_max, r_max = 0, 0
+    l_max, r_max = height[0], height[-1]
     volume = 0
 
-    while l < r:
-        if height[l] < height[r]:
-            if height[l] >= l_max:
-                l_max = height[l]
-            else:
-                volume += l_max - height[l]
+    while l <= r:
+        l_max = max(l_max, height[l])
+        r_max = max(r_max, height[r])
+
+        if l_max < r_max:
+            volume += l_max - height[l]
             l += 1
         else:
-            if height[r] >= r_max:
-                r_max = height[r]
-            else:
-                volume += r_max - height[r]
+            volume += r_max - height[r]
             r -= 1
 
     return volume
