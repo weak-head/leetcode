25b9390c868b017f1c15672d9183ab6b9ae112d7
Oleksandr Zinchenko <zinchenko@live.com>
2021-04-13 18:27:30 -0700

    1500 - design a file sharing system

--- leetcode/p1500_design_a_file_sharing_system.py
+++ leetcode/p1500_design_a_file_sharing_system.py
@@ -1,4 +1,4 @@
-from sortedcontainers import SortedSet
+import heapq
 from collections import defaultdict
 from typing import List
 
@@ -6,14 +6,14 @@ from typing import List
 class FileSharing:
     def __init__(self, m: int):
         self.next_id = 1
-        self.free_ids = SortedSet()
+        self.free_ids = []
         self.chunks = defaultdict(set)  # chunk -> [user]
         self.users = defaultdict(set)  # user -> [chunk]
 
     def join(self, ownedChunks: List[int]) -> int:
         user_id = None
         if self.free_ids:
-            user_id = self.free_ids.pop(0)
+            user_id = heapq.heappop(self.free_ids)
         else:
             user_id = self.next_id
             self.next_id += 1
@@ -28,7 +28,7 @@ class FileSharing:
         for chunk in self.users[userID]:
             self.chunks[chunk].remove(userID)
         del self.users[userID]
-        self.free_ids.add(userID)
+        heapq.heappush(self.free_ids, userID)
 
     def request(self, userID: int, chunkID: int) -> List[int]:
         users = sorted(self.chunks[chunkID])
