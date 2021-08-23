c3753b2c2827d4d201c9e5057b542e0be6d8e45a
Oleksandr Zinchenko <zinchenko@live.com>
2021-08-23 12:42:27 -0400

    0261 - graph valid tree

--- leetcode/p0261_graph_valid_tree.py
+++ leetcode/p0261_graph_valid_tree.py
@@ -2,7 +2,50 @@ from typing import List
 from collections import defaultdict, deque
 
 
-def validTree(n: int, edges: List[List[int]]) -> bool:
+def validTree_uf(n, edges):
+    """
+    Union find / Disjoint set
+
+    Time: O(v)
+    Space: O(v)
+    """
+
+    # Graph should have (n-1) edges to be a valid tree
+    if len(edges) != n - 1:
+        return False
+
+    parent = list(range(n))
+    rank = [1] * n
+
+    def find(x):
+        if x != parent[x]:
+            parent[x] = find(parent[x])
+        return parent[x]
+
+    def union(xy):
+        x, y = map(find, xy)
+
+        # cycle
+        if x == y:
+            return False
+
+        if rank[x] < rank[y]:
+            parent[x] = y
+        else:
+            parent[y] = x
+            if rank[x] != rank[y]:
+                rank[x] += 1
+
+        return True
+
+    for edge in edges:
+        if not union(edge):
+            return False
+
+    return True
+
+
+def validTree_bfs(n: int, edges: List[List[int]]) -> bool:
     """
     BFS
 
--- tests/test_p0261_graph_valid_tree.py
+++ tests/test_p0261_graph_valid_tree.py
@@ -3,12 +3,14 @@ import pytest
 from leetcode.p0261_graph_valid_tree import *
 
 solutions = [
-    validTree,
+    validTree_bfs,
+    validTree_uf,
 ]
 
 test_cases = [
     ([5, [[0, 1], [0, 2], [0, 3], [1, 4]]], True),
     ([5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]], False),
+    ([6, [(0, 5), (4, 0), (1, 2), (4, 5), (3, 2)]], False),
 ]
 
 
