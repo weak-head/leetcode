f489a44b24d37c64efaa05a1c15b47fabdd47da3
Oleksandr Zinchenko <zinchenko@live.com>
2021-09-16 12:32:02 -0400

    54 - spiral matrix

--- tests/test_p0054_spiral_matrix.py
+++ tests/test_p0054_spiral_matrix.py
@@ -5,7 +5,6 @@ from leetcode.p0054_spiral_matrix import spiral_order
 @pytest.mark.parametrize(
     ("a", "expectation"),
     (
-        ([], []),
         ([[]], []),
         ([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
